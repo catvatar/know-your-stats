ed9ecf3f0479f984ab2b92d2b03c3eee270708cc
catvatar <olekgogol@protonmail.com>
2024-10-28 11:50:45 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Stopwatches from './Stopwatches';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Know your stats
+      </h1>
+      <Stopwatches/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Stopwatches.tsx
@@ -0,0 +1,9 @@
+export default function Stopwatches() {
+    return <div>
+        <h2>Stopwatches</h2>
+        <ul>
+            <li>Stopwach 1</li>
+            <li>Stopwach 2</li>
+        </ul>
+    </div>
+}
