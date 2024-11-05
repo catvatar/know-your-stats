8cec20a84828d797ed12f6d5fdf5e38748097a8b
catvatar <olekgogl@gmail.com>
2024-11-05 19:24:06 +0100

    added new route

---
+++ client/src/EntriesBrowser.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function EntriesBrowser() {
+    return (
+        <div className="bg-gray-900 text-white min-h-screen p-8">
+            <h1 className="text-4xl font-bold underline mb-8 text-center">
+                Know your stats
+            </h1>
+        </div>
+    );
+}
