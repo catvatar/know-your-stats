1af1a3de8c10dbdde667b946e2b438f2211e8a3a
catvatar <olekgogl@gmail.com>
2024-11-05 11:11:42 +0100

    Rewrote code to use useReducer

--- client/src/components/Stopwatch.tsx
+++ client/src/components/Stopwatch.tsx
@@ -1,12 +1,8 @@
-import React, { useEffect, useState } from 'react'; 
+import React, { useEffect, useReducer, useState } from 'react'; 
 
 import { formatTime } from '../utils/time-formats';
 
-type StopwatchEntry = {
-    id: number;
-    start_time: number;
-    stop_time: number | null;
-}
+
 
 const fetchStopwatchEntries = async (id: number, limit: number | null = null) => {
     const apiURL: string = limit ? `http://localhost:3001/api/stopwatches/${id}/entries/${limit}` : `http://localhost:3001/api/stopwatches/${id}/entries`;
@@ -56,49 +52,85 @@ const stopStopwatch = async (id: number) => {
     });
 }
 
+type StopwatchEntry = {
+    id: number;
+    start_time: number;
+    stop_time: number | null;
+}
+
+interface StopwatchState {
+    stopwatchEntry: StopwatchEntry | null;
+    isRunning: boolean;
+    elapsedTime: number;
+}
+
+const initialStopwatchState: StopwatchState = {
+    stopwatchEntry: null,
+    isRunning: false,
+    elapsedTime: 0
+};
+
+function stopwatchReducer(state: StopwatchState, update: StopwatchEntry): StopwatchState {
+    const calculateStateFromEntry = (stopwatchEntry: StopwatchEntry) => {
+        const isRunning = stopwatchEntry.stop_time === null;
+        const elapsedTime = isRunning ? Date.now() - stopwatchEntry.start_time : stopwatchEntry.stop_time as number - stopwatchEntry.start_time;
+        return {
+            isRunning,
+            elapsedTime
+        };
+    };
+    const newState = calculateStateFromEntry(update);
+    return {
+        ...state,
+        ...newState,
+        stopwatchEntry: update
+    };
+}
+
 export default function Stopwatch({id}: {id: number}) {
-    const [lastStopwatchEntry, setLastStopwatchEntry] = useState<StopwatchEntry | null>(null);
-    const [isRunning, setIsRunning] = useState<boolean>(false);
-    const [elapsedTime, setElapsedTime] = useState<number>(0);
+    const [stopwatchState, stopwatchDispatchAction] = useReducer(stopwatchReducer,initialStopwatchState);
+    const [fakeTime, setFakeTime] = useState(0);
 
-    const fetchAndUpdateState = async () => {
-        fetchStopwatchEntries(id, 1).then((entries: StopwatchEntry[]) => {
-            if (entries.length == 0) {
-                setLastStopwatchEntry(null);
-                return;
-            }
-            setLastStopwatchEntry(entries[0]);
-            setIsRunning(entries[0].stop_time === null);
-            setElapsedTime(entries[0].stop_time ? entries[0].stop_time - entries[0].start_time : Date.now() - entries[0].start_time);
-        });
-    }
-    
     useEffect(() => {
-        fetchAndUpdateState().then(() => {
-            console.log("Fetched");
+        fetchStopwatchEntries(id, 1).then((entries: StopwatchEntry[]) => {
+            console.trace(entries);
+            if(entries.length > 0) {
+                stopwatchDispatchAction(entries[0]);
+            }
         });
-    }, [isRunning]);
+    }, []);
 
-    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if(stopwatchState.isRunning) {
+                setFakeTime((prev) => prev + 1000);
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [stopwatchState.isRunning]);
+
     const handleStart = () => {
         startStopwatch(id).then(() => {
-            fetchAndUpdateState().then(() => {
-                console.log("Fetched");
+            fetchStopwatchEntries(id, 1).then((entries: StopwatchEntry[]) => {
+                if(entries.length > 0) {
+                    stopwatchDispatchAction(entries[0]);
+                }
             });
         });
-        console.log("Start");
-    };
+    }
 
     const handleStop = () => {
         console.log("Stop");
-        // setIsRunning(false);
-    };
-
+    }
 
     return <div className="flex flex-col items-center p-4 bg-gray-800 text-white rounded shadow-md">
-        <p className="text-2xl mb-4">{lastStopwatchEntry ? formatTime(elapsedTime || 0) : "Run the stopwatch by pressing the Start button"}</p>
+        <p className="text-2xl mb-4">{stopwatchState.elapsedTime > 0 ? formatTime(stopwatchState.elapsedTime + fakeTime)  : "Run the stopwatch by pressing the Start button"}</p>
+        <p className="text-lg mb-4">{`Start time: ${stopwatchState.stopwatchEntry?.start_time}`}</p>
+        <p className='text-lg mb-4'>{`Current time: ${Date.now()}`}</p>
+        <p className='text-lg mb-4'>{`Elapsed time: ${stopwatchState.elapsedTime}`}</p>
+        <p className='text-lg mb-4'>{`Fake time: ${fakeTime}`}</p>
         <div className="space-x-2">
-            {!isRunning?
+            {!stopwatchState.isRunning?
             <button onClick={handleStart} className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700">Start</button>:
             <button onClick={handleStop} className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700">Stop</button>
             }
